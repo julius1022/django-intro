cd910b11fc4ed15f36a4fd3503252cae648c0cd1
Hatibu <juliushatibu@gmail.com>
2023-03-10 14:00:40 +0300

    added templates

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>This is a test page</h1>')
+    return render(request, '../templates/pages/index.html')
+def about(request):
+    return render(request, '../templates/pages/about.html')
